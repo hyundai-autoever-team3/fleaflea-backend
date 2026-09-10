a988a5a7fb0ba693f192c2b17e2b678391b40b2e
sor999 <sorsor999@naver.com>
2026-09-10 17:45:16 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/com/anabada/fleaflea/FleafleaApplication.java
@@ -0,0 +1,13 @@
+package com.anabada.fleaflea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleafleaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleafleaApplication.class, args);
+	}
+
+}
