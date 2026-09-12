4e5d96074d88331283c002a7322fadf0562d0269
paulgks-design <paulgks@gmail.com>
2026-09-12 19:21:37 +0900

    fix: 플리마켓 참여 멤버 -> CreatedDate 제거

--- src/main/java/com/anabada/fleaflea/domain/marketmember/domain/MarketMember.java
+++ src/main/java/com/anabada/fleaflea/domain/marketmember/domain/MarketMember.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -23,7 +24,6 @@ public class MarketMember {
     @Column(nullable = false)
     private Long memberId;
 
-    @CreatedDate
     @Column(name = "joined_at", nullable = false)
     private LocalDateTime joinedAt;
 }
