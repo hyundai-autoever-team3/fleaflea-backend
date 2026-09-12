1079ccd06cc8b3f42c576044372705d83004de5e
sor999 <sorsor999@naver.com>
2026-09-12 15:04:47 +0900

    feat: 거래 엔티티 추가

---
+++ src/main/java/com/anabada/fleaflea/domain/trade/domain/Trade.java
@@ -0,0 +1,39 @@
+package com.anabada.fleaflea.domain.trade.domain;
+
+import com.anabada.fleaflea.global.entity.BaseCreatedTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "trades")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Trade extends BaseCreatedTimeEntity {
+
+    @Id
+    private Long tradeId;
+
+    @Column(nullable = false)
+    private Long collectionTradeRequestId;
+
+    @Column(nullable = false)
+    private Long tradeRequestId;
+
+    @Column(nullable = false)
+    private Long itemId;
+
+    @Column(nullable = false)
+    private Long buyerId;
+
+    @Column(nullable = false)
+    private Long sellerId;
+
+    private LocalDateTime completedAt;
+}
