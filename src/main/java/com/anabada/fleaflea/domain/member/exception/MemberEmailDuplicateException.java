a5ab9d730ab76b40d86573a5eccf55b19f5f5e3a
paulgks-design <paulgks@gmail.com>
2026-09-13 12:01:00 +0900

    feat: 멤버 이메일 중복 예외 추가

---
+++ src/main/java/com/anabada/fleaflea/domain/member/exception/MemberEmailDuplicateException.java
@@ -0,0 +1,10 @@
+package com.anabada.fleaflea.domain.member.exception;
+
+import com.anabada.fleaflea.global.exception.BusinessException;
+import com.anabada.fleaflea.global.exception.ErrorCode;
+
+public class MemberEmailDuplicateException extends BusinessException {
+    public MemberEmailDuplicateException() {
+        super(ErrorCode.MEMBER_EMAIL_DUPLICATE);
+    }
+}
--- src/main/java/com/anabada/fleaflea/global/exception/ErrorCode.java
+++ src/main/java/com/anabada/fleaflea/global/exception/ErrorCode.java
@@ -13,7 +13,8 @@ public enum ErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "서버 내부 오류입니다."),
 
     // Member
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_NOT_FOUND", "존재하지 않는 사용자입니다.");
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_NOT_FOUND", "존재하지 않는 사용자입니다."),
+    MEMBER_EMAIL_DUPLICATE(HttpStatus.CONFLICT, "MEMBER_EMAIL_DUPLICATE", "이미 존재하는 이메일입니다.");
 
     private final HttpStatus status;
     private final String code;
