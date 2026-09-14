dd0fb9b36d244ad54b8d94548e256a67cdc87112
SeungJun_Song <john091122@naver.com>
2026-09-14 19:12:51 +0900

    feat: 플리마켓 엔티티 연관관계 구성

--- src/main/java/com/anabada/fleaflea/domain/market/domain/Market.java
+++ src/main/java/com/anabada/fleaflea/domain/market/domain/Market.java
@@ -1,13 +1,10 @@
 package com.anabada.fleaflea.domain.market.domain;
 
+import com.anabada.fleaflea.domain.member.domain.Member;
 import com.anabada.fleaflea.global.entity.BaseTimeEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,8 +18,9 @@ public class Market extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long marketId;
 
-    @Column(nullable = false)
-    private Long hostId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "host_id", nullable = false)
+    private Member host;
 
     @Column(nullable = false, length = 100)
     private String title;
@@ -30,6 +28,42 @@ public class Market extends BaseTimeEntity {
     @Column(columnDefinition = "TEXT")
     private String description;
 
-    @Column(length = 100)
+    @Column(nullable = false, unique = true, length = 100)
     private String inviteCode;
+
+    @Builder
+    private Market(
+            Member host,
+            String title,
+            String description,
+            String inviteCode
+    ) {
+        this.host = host;
+        this.title = title;
+        this.description = description;
+        this.inviteCode = inviteCode;
+    }
+
+    public static Market create(
+            Member host,
+            String title,
+            String description,
+            String inviteCode
+    ) {
+        return Market.builder()
+                .host(host)
+                .title(title)
+                .description(description)
+                .inviteCode(inviteCode)
+                .build();
+    }
+
+    public void update(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public void changeInviteCode(String inviteCode) {
+        this.inviteCode = inviteCode;
+    }
 }
