9afdc214c5c181b574536c400dad001cfa2cef74
paulgks-design <paulgks@gmail.com>
2026-09-14 14:45:07 +0900

    feat: JWT 토큰 예외 추가

--- src/main/java/com/anabada/fleaflea/global/exception/ErrorCode.java
+++ src/main/java/com/anabada/fleaflea/global/exception/ErrorCode.java
@@ -16,8 +16,10 @@ public enum ErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_NOT_FOUND", "존재하지 않는 사용자입니다."),
     DUPLICATE_NICKNAME(HttpStatus.CONFLICT, "DUPLICATE_NICKNAME", "이미 존재하는 닉네임입니다."),
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "DUPLICATE_EMAIL", "이미 존재하는 이메일입니다."),
-    INVALID_LOGIN(HttpStatus.UNAUTHORIZED, "INVALID_LOGIN", "이메일 또는 비밀번호가 올바르지 않습니다");
+    INVALID_LOGIN(HttpStatus.UNAUTHORIZED, "INVALID_LOGIN", "이메일 또는 비밀번호가 올바르지 않습니다"),
 
+    // Refresh Token
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "INVALID_TOKEN", "유효하지 않은 토큰입니다.");
     private final HttpStatus status;
     private final String code;
     private final String message;
