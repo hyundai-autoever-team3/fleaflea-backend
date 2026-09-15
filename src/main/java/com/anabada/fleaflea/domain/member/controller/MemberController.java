5c0925a24a792a5e0b1c5d2f7156225cc02250a3
paulgks-design <paulgks@gmail.com>
2026-09-15 11:19:59 +0900

    chore: API URL 에 prefix 적용

--- src/main/java/com/anabada/fleaflea/domain/member/controller/MemberController.java
+++ src/main/java/com/anabada/fleaflea/domain/member/controller/MemberController.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/members")
+@RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
