1b9df33327ae53f85f8107ae8e4d1e84e2a20c9a
sor999 <sorsor999@naver.com>
2026-09-17 00:54:28 +0900

    feat: 알림 타입 추가

---
+++ src/main/java/com/anabada/fleaflea/domain/notification/domain/NotificationReferenceType.java
@@ -0,0 +1,8 @@
+package com.anabada.fleaflea.domain.notification.domain;
+
+public enum NotificationReferenceType {
+    ITEM_TRADE_REQUEST,
+    COLLECTION_TRADE_REQUEST,
+    BEG_REQUEST,
+    FRIEND_REQUEST
+}
