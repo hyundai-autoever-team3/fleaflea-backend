74349de2a693b7284f8acaa40581f71f79d01516
paulgks-design <paulgks@gmail.com>
2026-09-18 14:35:43 +0900

    fix: DB 마이그레이션 수정

--- src/main/resources/db/migration/V6__add_beg_request_id_to_trades.sql
+++ src/main/resources/db/migration/V6__add_beg_request_id_to_trades.sql
@@ -6,8 +6,12 @@ ALTER TABLE trades
         FOREIGN KEY (beg_request_id)
             REFERENCES beg_requests (beg_request_id);
 
+ALTER TABLE trades
+    ADD CONSTRAINT uk_trades_beg_request_id
+        UNIQUE (beg_request_id);
+
 ALTER TABLE beg_requests
-DROP CONSTRAINT ck_beg_requests_status;
+DROP CONSTRAINT IF EXISTS ck_beg_requests_status;
 
 ALTER TABLE beg_requests
     ADD CONSTRAINT ck_beg_requests_status
