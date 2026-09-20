3574063a1bc042ce68fb3d739bea3e9b3e46c83b
paulgks-design <paulgks@gmail.com>
2026-09-20 14:23:54 +0900

    refactor: Friendship 조회 로직 QueryDsl 전환

--- src/main/java/com/anabada/fleaflea/domain/friendship/repository/FriendshipRepository.java
+++ src/main/java/com/anabada/fleaflea/domain/friendship/repository/FriendshipRepository.java
@@ -11,20 +11,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface FriendshipRepository
-        extends JpaRepository<Friendship, Long> {
+        extends JpaRepository<Friendship, Long>, FriendshipRepositoryCustom {
 
-    List<Friendship> findByAddressee_MemberIdAndStatus(
-            Long memberId,
-            FriendshipStatus status
-    );
 
-    List<Friendship> findByRequester_MemberIdAndStatus(
-            Long memberId,
-            FriendshipStatus status
-    );
-
-    Optional<Friendship>
-    findByRequester_MemberIdAndAddressee_MemberIdAndStatus(
+    Optional<Friendship> findByRequester_MemberIdAndAddressee_MemberIdAndStatus(
             Long requesterId,
             Long addresseeId,
             FriendshipStatus status
@@ -41,21 +31,6 @@ public interface FriendshipRepository
             Collection<FriendshipStatus> statuses
     );
 
-    @Query("""
-            SELECT f
-            FROM Friendship f
-            JOIN FETCH f.requester
-            JOIN FETCH f.addressee
-            WHERE f.status = :status
-              AND (
-                    f.requester.memberId = :memberId
-                    OR f.addressee.memberId = :memberId
-              )
-            """)
-    List<Friendship> findByFriendships(
-            @Param("memberId") Long memberId,
-            @Param("status") FriendshipStatus status
-    );
 
     boolean existsByRequester_MemberIdAndAddressee_MemberIdAndStatus(
             Long requesterId,
---
+++ src/main/java/com/anabada/fleaflea/domain/friendship/repository/FriendshipRepositoryCustom.java
@@ -0,0 +1,14 @@
+package com.anabada.fleaflea.domain.friendship.repository;
+
+import com.anabada.fleaflea.domain.friendship.domain.Friendship;
+
+import java.util.List;
+
+public interface FriendshipRepositoryCustom {
+    List<Friendship> findReceivedRequests(Long memberId);
+
+    List<Friendship> findSentRequests(Long memberId);
+
+    List<Friendship> findFriends(Long memberId);
+
+}
